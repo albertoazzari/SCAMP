cf5f3e93e036525332a2f25ce2bb51d56a41e043
Alberto Azzari <alberto.azzari@univr.it>
2024-03-06 18:30:37 +0100

    added extern for binding

--- src/common/api.h
+++ src/common/api.h
@@ -1,3 +1,11 @@
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void compute_scamp(const double* ts, size_t ts_len, int window_size, float* mp, int* indexes);
+
+#ifdef __cplusplus
+}
+#endif
